07dc536ab836f9d6c87d49c89f9b1df32839bf5c
Asep Fajar Firmansyah <asep.airlangga@uinjkt.ac.id>
2022-03-07 13:41:27 +0100

    modified:   multilingual_nmt_t5.py

--- multilingual_nmt_t5.py
+++ multilingual_nmt_t5.py
@@ -269,7 +269,7 @@ model_params={
     "TRAIN_EPOCHS":3,              # number of training epochs
     "VAL_EPOCHS":1,                # number of validation epochs
     "LEARNING_RATE":1e-4,          # learning rate
-    "MAX_SOURCE_TEXT_LENGTH":125,  # max length of source text
+    "MAX_SOURCE_TEXT_LENGTH":36,  # max length of source text
     "MAX_TARGET_TEXT_LENGTH":125,   # max length of target text
     "SEED": 42                     # set seed for reproducibility 
 
